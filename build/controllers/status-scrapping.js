dad46046038a477650c009f03c23d5e7033abbda
Kevin <kevinstevenzeasuarez@gmail.com>
2023-11-18 20:16:38 -0500

    more time for ver

--- build/controllers/status-scrapping.js
+++ build/controllers/status-scrapping.js
@@ -3,6 +3,7 @@ import { JSDOM } from 'jsdom';
 
 export async function getStatus(link) {
     try {
+        setTimeout(20000)
         const response = await fetch(link, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
